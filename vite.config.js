8127a44c241ee66390bc5696e70aeb3127c653ec
Yuval <100403800+Yuvif@users.noreply.github.com>
2025-07-21 14:48:42 +0300

    remove size indicator

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+
 
 const repoName = 'yarden-site';
 
 export default defineConfig({
   base: `/${repoName}/`,
-  plugins: [
-    react(),
-    visualizer({
-      filename: 'dist/stats.html',
-      open: true,
-    }),
-  ],
+  plugins: [react()],
 });
